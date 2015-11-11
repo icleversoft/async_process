32eb7fdebad1d6d651d44d4bfb7f59d2dea7406f
icleversoft <iphone@icleversoft.com>
2015-11-11 09:39:33 +0200

    refactoring files_worker class

--- lib/async_process/files_worker.rb
+++ lib/async_process/files_worker.rb
@@ -1,16 +1,16 @@
 module AsyncProcess  
 	class FilesWorker
-		def initialize( scanner, on_file_process = nil, on_process_end = nil )
+		def initialize( scanner )
 			@scanner = scanner
-			@on_process = on_file_process
-			@on_end = on_process_end
 		end
 
-		def process_folder(folder)
+		def process_folder(folder, &block)
 			@scanner.files_for_folder( folder ) do |file|
-				@on_process.call( file  ) if @on_process
+        block.call_event :file_in_folder, file, folder
+				#@on_process.call( file  ) if @on_process
 			end
-			@on_end.call if @on_end
+      block.call_event :folder_end, folder
+			#@on_end.call if @on_end
 		end
 	end
 end
--- spec/lib/files_worker_spec.rb
+++ spec/lib/files_worker_spec.rb
@@ -2,19 +2,25 @@ require 'spec_helper'
 module AsyncProcess
   describe FilesWorker do
     let(:scanner){FileScanner.new( File.join(File.dirname(__FILE__), '../support', 'root' ) )}
-    let(:on_file){lambda{|file| puts "Processing file: #{file}..."}}
-    let(:on_end){lambda{puts "Process finished"}}
-    let(:worker){FilesWorker.new(scanner, on_file, on_end)}
+    let(:worker){FilesWorker.new(scanner)}
 
     it "responds to process_folder" do
       expect(worker).to respond_to :process_folder
     end
-    
+
     context "#process_folder" do
       it "process correctly a folder" do
-        expect{worker.process_folder("01")}.to output("Processing file: /Users/gstavrou/Development/rails/async_process/spec/lib/../support/root/01/file4.txt...\nProcess finished\n").to_stdout
+        expect{
+          worker.process_folder("01") do |on|
+            on.file_in_folder do |file, folder|
+              print "Processing file: #{file}...\n"
+            end
+            on.folder_end do |folder|
+              print "Process finished"
+            end
+          end
+        }.to output("Processing file: /Users/gstavrou/Development/rails/async_process/spec/lib/../support/root/01/file4.txt...\nProcess finished").to_stdout
       end
     end
-
   end
 end
